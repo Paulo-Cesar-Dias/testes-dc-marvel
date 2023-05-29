d0bb6b1c0a664d0291f5fc1aaceb8a37ee3ebe18
Paulo César Dias Rufino <paulocesar04122004@gmail.com>
2023-05-29 16:20:22 -0300

    comecando

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+$perguntas=array();
+$perguntas["questão1"]=array("a"=>false,"b"=>false,"c"=>true,"d"=>false);
+
+
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Planejamento</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="container-fluid">
+
+<form method="post" action="index.php">
+
+<input type="text" name="questao1"><br>
+
+<input type="submit" name="responder">
+
+</form>
+
+
+
+</div>
+</body>
+</html>
