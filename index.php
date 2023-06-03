19ae1ea80870214ee1b80756b5ab362fa5827bfa
paulo cesar <paulocesar04122004@gmail.com>
2023-06-02 21:32:03 -0300

    functions questoes

--- index.php
+++ index.php
@@ -60,73 +60,194 @@
 
     }
 
- 
-/*
-$perguntas[1]["questao"]="2. Qual o nome de nascença do Superman?";
-$perguntas[1][0]=array("Kal-El",true);
-$perguntas[1][1]=array("Clark Kent",false);
-$perguntas[1][2]=array("Jor-El",false);
-$perguntas[1][3]=array("Virgil Hawkins",false);
+function questao2($alternativa){
 
-if ($perguntas[1][$alternativa][1]==true){
+    $perguntas[1]["questao"]="2. Qual o nome de nascença do Superman?";
+    $perguntas[1][0]=array("Kal-El",true);
+    $perguntas[1][1]=array("Clark Kent",false);
+    $perguntas[1][2]=array("Jor-El",false);
+    $perguntas[1][3]=array("Virgil Hawkins",false);
+    if(is_numeric($alternativa)){
+
+        if ($perguntas[1][$alternativa][1]==true){
             echo "Acertou";
         }else{
             echo "Errou";
         }
-        
-        
+    }
+        echo"<form method='post' action='index.php'><label>"
+        . $perguntas[1]["questao"]."</label><br>
+        <input type='radio' name='alternativa' value='0'>
+        <label>"
+        . $perguntas[1][0][0]."</label><br>
+        <input type='radio' name='alternativa' value='1'>
+        <label>"
+        . $perguntas[1][1][0] .
+        " </label><br>
+        <input type='radio' name='alternativa' value='2'>
+        <label>"
+        . $perguntas[1][2][0].
+        "</label><br>
+        <input type='radio' name='alternativa' value='3'>
+        <label>"
+        . $perguntas[1][3][0].
+        "</label><br><br>
+        <input type='submit' name='calcular'>
+        </form>";
+
+    } 
+    function questao3($alternativa){
+
         $perguntas[2]["questao"]="3. Qual o primeiro vilão enfrentado pela liga da justiça?";
         $perguntas[2][0]=array("Prometheus, o gênio criminal",false);
         $perguntas[2][1]=array("Starro, a estrela alienígena gigante",true);
         $perguntas[2][2]=array("Darkseid",false);
         $perguntas[2][3]=array("Amazo, android imitador de poderes",false);
-        
-        if ($perguntas[2][$alternativa][1]==true){
-            echo "Acertou";
-        }else{
+        if (is_numeric($alternativa)) {
+            # code...
+            if ($perguntas[2][$alternativa][1]==true){
+                echo "Acertou";
+            }else{
             echo "Errou";
-        }
+            }
+        }        
 
+        echo"<form method='post' action='index.php'><label>"
+        . $perguntas[2]["questao"]."</label><br>
+        <input type='radio' name='alternativa' value='0'>
+        <label>"
+        . $perguntas[2][0][0]."</label><br>
+        <input type='radio' name='alternativa' value='1'>
+        <label>"
+        . $perguntas[2][1][0] .
+        " </label><br>
+        <input type='radio' name='alternativa' value='2'>
+        <label>"
+        . $perguntas[2][2][0].
+        "</label><br>
+        <input type='radio' name='alternativa' value='3'>
+        <label>"
+        . $perguntas[2][3][0].
+        "</label><br><br>
+        <input type='submit' name='calcular'>
+        </form>";
+    }
         
+    function questao4($alternativa){
+
         $perguntas[3]["questao"]="4. Quais dessas heroínas são fundadoras (principais) da liga da justiça sem limites?";
         $perguntas[3][0]=array("Mulher maravilha e Canário negro",false);
         $perguntas[3][1]=array("Canário negro e Caçadora",false);
         $perguntas[3][2]=array("Mulher gavião e Caçadora",false);
         $perguntas[3][3]=array("Mulher maravilha e Mulher gavião",true);
+        if (is_numeric($alternativa)) {
+            # code...
+            if ($perguntas[3][$alternativa][1]==true){
+                echo "Acertou";
+            }else{
+                echo "Errou";
+            }
+        }        
         
-        if ($perguntas[3][$alternativa][1]==true){
-            echo "Acertou";
-        }else{
-            echo "Errou";
-        }
+        echo"<form method='post' action='index.php'><label>"
+        . $perguntas[3]["questao"]."</label><br>
+        <input type='radio' name='alternativa' value='0'>
+        <label>"
+        . $perguntas[3][0][0]."</label><br>
+        <input type='radio' name='alternativa' value='1'>
+        <label>"
+        . $perguntas[3][1][0] .
+        " </label><br>
+        <input type='radio' name='alternativa' value='2'>
+        <label>"
+        . $perguntas[3][2][0].
+        "</label><br>
+        <input type='radio' name='alternativa' value='3'>
+        <label>"
+        . $perguntas[3][3][0].
+        "</label><br><br>
+        <input type='submit' name='calcular'>
+        </form>";
+
         
-        
+    }
+
+    function questao5($alternativa){
+
         $perguntas[4]["questao"]="5. Sobre a Estelar, qual o seu planeta natal?";
         $perguntas[4][0]=array("Mulher maravilha e Canário negro",false);
         $perguntas[4][1]=array("Canário negro e Caçadora",false);
         $perguntas[4][2]=array("Mulher gavião e Caçadora",false);
         $perguntas[4][3]=array("Mulher maravilha e Mulher gavião",true);
-        
-        if ($perguntas[4][$alternativa][1]==true){
-            echo "Acertou";
-        }else{
-            echo "Errou";
+        if (is_numeric($alternativa)) {
+            # code...
+            if ($perguntas[4][$alternativa][1]==true){
+                echo "Acertou";
+            }else{
+                echo "Errou";
+            }
         }
-        
-        
+        echo"<form method='post' action='index.php'><label>"
+        . $perguntas[4]["questao"]."</label><br>
+        <input type='radio' name='alternativa' value='0'>
+        <label>"
+        . $perguntas[4][0][0]."</label><br>
+        <input type='radio' name='alternativa' value='1'>
+        <label>"
+        . $perguntas[4][1][0] .
+        " </label><br>
+        <input type='radio' name='alternativa' value='2'>
+        <label>"
+        . $perguntas[4][2][0].
+        "</label><br>
+        <input type='radio' name='alternativa' value='3'>
+        <label>"
+        . $perguntas[4][3][0].
+        "</label><br><br>
+        <input type='submit' name='calcular'>
+        </form>";
+    }
+    function questao6($alternativa){
         $perguntas[5]["questao"]="5. Qual a fraqueza da mulher maravilha?";
         $perguntas[5][0]=array("Kryptonita",false);
         $perguntas[5][1]=array("Magia de Ares",true);
         $perguntas[5][2]=array("Cloreto de etila",false);
         $perguntas[5][3]=array("Emoções",false);
-        
-        if ($perguntas[5][$alternativa][1]==true){
-            echo "Acertou";
-        }else{
-            echo "Errou";
+        if (is_numeric($alternativa)) {
+            # code...
+            if ($perguntas[5][$alternativa][1]==true){
+                echo "Acertou";
+            }else{
+                echo "Errou";
+            }
         }
+        echo"<form method='post' action='index.php'><label>"
+        . $perguntas[5]["questao"]."</label><br>
+        <input type='radio' name='alternativa' value='0'>
+        <label>"
+        . $perguntas[5][0][0]."</label><br>
+        <input type='radio' name='alternativa' value='1'>
+        <label>"
+        . $perguntas[5][1][0] .
+        " </label><br>
+        <input type='radio' name='alternativa' value='2'>
+        <label>"
+        . $perguntas[5][2][0].
+        "</label><br>
+        <input type='radio' name='alternativa' value='3'>
+        <label>"
+        . $perguntas[5][3][0].
+        "</label><br><br>
+        <input type='submit' name='calcular'>
+        </form>";    
+    }
+        /*
         */
-        ?>
+
+        
+        /* Botões de voltar e avançar */
+        
+?>
         
         <!DOCTYPE html>
 <html lang="pt-BR">
@@ -140,8 +261,9 @@ if ($perguntas[1][$alternativa][1]==true){
 
 <body>
 <div class="container-fluid">
-<?php questao1($alternativa)  ?>
-
+<?php questao1($alternativa)?><br>
+<input type="button" name="navegacao" value="< volta">
+<input type="button" name="navegacao" value="próxima >">
 </div>
 </body>
 </html>
