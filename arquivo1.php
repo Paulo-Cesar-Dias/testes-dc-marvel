adb484bfa466fb7cec409c32dbf00b3e732b2105
diogofariasz <diogo.farias@escolar.ifrn.edu.br>
2023-06-12 08:27:19 -0300

    ajeitei o recomeçar!

--- arquivo1.php
+++ arquivo1.php
@@ -200,7 +200,7 @@
                 <center><p id='pontuacao'><br>
                 Parabens ".$mais_homi."!!! Você acertou
                 " . $ponto ." de 10</p></center>
-                <a href='#questao0'> Recomeçar </a>
+                <a href='#questao1'> Recomeçar </a>
         
             </div></center> 
             "; 
@@ -268,7 +268,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quiz</title>
+    <title>QUIZ</title>
     <link rel="stylesheet" href="style1.css">
 </head>
 <style>
